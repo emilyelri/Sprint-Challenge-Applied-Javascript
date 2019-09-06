47ef014774680d9f029152cc157ea4f3e418a77f
Emily_Richard <richard.emily.e@gmail.com>
2019-09-06 12:04:16 -0400

    Finished cards index.js

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,51 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+const cardsContainer = document.querySelector(".cards-container");
+
+function createCard(article) {
+  const card = document.createElement("div"),
+    headline = document.createElement("div"),
+    author = document.createElement("div"),
+    imgContainer = document.createElement("div"),
+    authorImage = document.createElement("img"),
+    byAuthor = document.createElement("span");
+
+  card.classList.add("card");
+  headline.classList.add("headline");
+  author.classList.add("author");
+  imgContainer.classList.add("img-container");
+
+  card.appendChild(headline);
+  card.appendChild(author);
+  author.appendChild(imgContainer);
+  imgContainer.appendChild(authorImage);
+  author.appendChild(byAuthor);
+
+  headline.textContent = article.headline;
+  authorImage.src = article.authorPhoto;
+  byAuthor.textContent = `By ${article.authorName}`;
+
+  return card;
+}
+
+axios
+  .get("https://lambda-times-backend.herokuapp.com/articles")
+  .then(response => {
+    console.log(response);
+    const object = response.data.articles;
+    // console.log(object);
+    for (var key in object) {
+      // console.log(key);
+      if (object.hasOwnProperty(key)) {
+        const articleArray = object[key];
+        // console.log(object[key]);
+        articleArray.forEach(article => {
+          // console.log(article);
+          const newCard = createCard(article);
+          cardsContainer.appendChild(newCard);
+        });
+      }
+    }
+  });
