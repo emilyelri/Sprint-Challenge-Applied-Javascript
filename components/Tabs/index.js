d4bf23a3e4cd693243e4d37fa75c31b109fc185a
Emily_Richard <richard.emily.e@gmail.com>
2019-09-06 11:24:16 -0400

    Finished tabs index.js

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,24 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+const topicsContainer = document.querySelector(".topics");
+
+function topicCard(topic) {
+  const tab = document.createElement("div");
+  tab.classList.add("tab");
+  tab.textContent = topic;
+
+  return tab;
+}
+
+axios
+  .get("https://lambda-times-backend.herokuapp.com/topics")
+  .then(response => {
+    console.log(response);
+
+    response.data.topics.forEach(topic => {
+      const newTopic = topicCard(topic);
+      topicsContainer.appendChild(newTopic);
+    })
+})
